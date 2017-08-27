1da057b649b06e49cd8e98b853c62a1624d4df95
Alex Rosier <alex.rosier@goudengids.be>
2017-08-27 12:49:40 +0200

    Exercises

--- src/main/java/com/axxes/persistence/dao/LibraryUserDao.java
+++ src/main/java/com/axxes/persistence/dao/LibraryUserDao.java
@@ -1,12 +1,21 @@
 package com.axxes.persistence.dao;
 
 import com.axxes.persistence.domain.LibraryUser;
+import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 @Component
 public class LibraryUserDao {
+
+    @Autowired
+    private ConnectionManager connectionManager;
 
     public List<LibraryUser> getUsersWithSuggestion() {
         return null;
@@ -21,17 +30,66 @@ public class LibraryUserDao {
     }
 
     public LibraryUser findUserWithFirstName(String firstName) {
+        try {
+            Connection connection = connectionManager.openConnection();
+            Statement statement = connection.createStatement();
+            String sql = "SELECT * FROM libraryuser WHERE (firstname = '" + firstName + "')";
+            ResultSet resultSet = statement.executeQuery(sql);
+            if(resultSet.next()) {
+                LibraryUser libraryUser = buildLibraryUser(resultSet);
+                connectionManager.closeConnection();
+                return libraryUser;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        connectionManager.closeConnection();
         return null;
     }
 
-    public List<LibraryUser> getAllUsers() {
-        return null;
+    public List<LibraryUser> getAllUsers()  {
+        try {
+            Connection connection = connectionManager.openConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM libraryuser");
+
+            List<LibraryUser> libraryUserList = Lists.newArrayList();
+            while (resultSet.next()) {
+                LibraryUser libraryUser = buildLibraryUser(resultSet);
+                libraryUserList.add(libraryUser);
+            }
+            return libraryUserList;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        connectionManager.closeConnection();
+        return Lists.newArrayListWithCapacity(0);
     }
 
     public LibraryUser updateUserLastName(long id, String lastName) {
+        try {
+            Connection connection = connectionManager.openConnection();
+            Statement statement = connection.createStatement();
+            String sql = "UPDATE libraryuser set lastname = '" + lastName + "' WHERE (id = '" + id + "')";
+            statement.executeUpdate(sql);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        connectionManager.closeConnection();
+//        return getUser();
         return null;
     }
 
     public void removeUser(long id) {
+    }
+
+    private LibraryUser buildLibraryUser(ResultSet resultSet) throws SQLException {
+        return new LibraryUser.Builder()
+                .setId(resultSet.getLong("id"))
+                .setEmail(resultSet.getString("email"))
+                .setUserName(resultSet.getString("userName"))
+                .setFirstName(resultSet.getString("firstName"))
+                .setLastName(resultSet.getString("lastName"))
+                .build();
     }
 }
