e6012bba5310f5c93cb03ed977924a226cee045d
Alex Rosier <alex.rosier@goudengids.be>
2017-08-28 05:39:07 +0200

    Exercises

--- src/main/java/com/axxes/persistence/dao/LibraryUserDao.java
+++ src/main/java/com/axxes/persistence/dao/LibraryUserDao.java
@@ -3,8 +3,11 @@ package com.axxes.persistence.dao;
 import com.axxes.persistence.domain.LibraryUser;
 import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -17,6 +20,9 @@ public class LibraryUserDao {
     @Autowired
     private ConnectionManager connectionManager;
 
+    @Autowired
+    private EntityManager entityManager;
+
     public List<LibraryUser> getUsersWithSuggestion() {
         return null;
     }
@@ -25,8 +31,9 @@ public class LibraryUserDao {
 
     }
 
+    @Transactional
     public void createUser(LibraryUser libraryUser) {
-
+        entityManager.persist(libraryUser);
     }
 
     public LibraryUser findUserWithFirstName(String firstName) {
--- src/main/java/com/axxes/persistence/dao/SuggestionDao.java
+++ src/main/java/com/axxes/persistence/dao/SuggestionDao.java
@@ -1,15 +1,22 @@
 package com.axxes.persistence.dao;
 
 import com.axxes.persistence.domain.Suggestion;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 
 @Component
 public class SuggestionDao {
 
-    public void createSuggestion(Suggestion suggestion) {
+    @Autowired
+    private EntityManager entityManager;
 
+    @Transactional
+    public void createSuggestion(Suggestion suggestion) {
+        entityManager.persist(suggestion);
     }
 
     public List<Suggestion> getAllSuggestion(Object p0) {
